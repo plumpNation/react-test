607a91a07955697d793ae383c4cfc8a8de01a127
Gavin King <gavin.king@fronter.com>
2014-08-15 00:58:15 +0200

    Some interaction and state

--- README.md
+++ README.md
@@ -5,4 +5,3 @@ A quick explore into the world of ReactJS, Browserify and Gulp.
 
 ## Thanks to the following for learning resources
 [Arne Martin and the Web Rebels crew](http://webrebels.23video.com/not-mobile-unfriendly-ui-components-with)
-[Browserify and watchify recipe](https://github.com/gulpjs/gulp/blob/master/docs/recipes/fast-browserify-builds-with-watchify.md)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 var React = require('react');
 
 var testComponent = React.createClass({
+  getInitialState: function () {
+    return {
+      clicks: 0
+    };
+  },
+  handleClick: function () {
+    this.setState({
+      clicks: this.state.clicks + 1
+    });
+  },
   render: function () {
-    return React.DOM.div(null, 'Hello world');
+    return React.DOM.div(null,
+      React.DOM.button(
+        {
+          onClick: this.handleClick,
+        },
+        'I have been clicked ', this.state.clicks, ' times'
+      )
+    );
   }
 });
 
